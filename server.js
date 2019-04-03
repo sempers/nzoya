0e476ea0bb1237a6f10a2be8b66f429af3c098c1
smart_rookie <groddenator@gmail.com>
2019-04-03 18:44:20 +0300

    0304

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
+const cons = require("consolidate");
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
+
 const app = express();
+
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 
-//app.engine("html", cons.ejs);
-//app.set('view engine', 'html');
-//app.set("views", path.resolve(__dirname, 'client/views'));
+app.engine("html", cons.ejs);
+app.set('view engine', 'html');
+app.set("views", __dirname);
 
 
 // send the user to index html page inspite of the url
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => console.log("nzoya server is listening on port "+ port));
